8df2301352c7b36103b62319315c702630db6eba
thebooleanguy <tharindus0601@gmail.com>
2024-12-19 21:08:09 +0530

    Detect compressed, video and image files in directory

--- tidyup.rb
+++ tidyup.rb
@@ -1,3 +1,16 @@
 # A program to sort your downloads folder
 
+dirName = ARGV[0].to_s
 
+
+unless dirName.empty?
+  Dir.chdir dirName
+end
+
+compressed_files = Dir.glob "*.{zip,rar,7z,tgz,gzip}"
+video_files = Dir.glob "*.{mp4,mkv,avi}"
+image_files = Dir.glob "*.{png,bmp,svg,jpeg,jpg}"
+
+#puts compressed_files
+#puts video_files
+puts image_files
