e1466d736a4ede03c64de2b9c3d46f9c9fc15b7b
thebooleanguy <tharindus0601@gmail.com>
2024-12-20 10:23:43 +0530

    Implement move functionality

--- tidyup.rb
+++ tidyup.rb
@@ -1,5 +1,7 @@
 # A program to organize your downloads directory by file type
 
+require 'fileutils'
+
 # Change working directory if argument provided
 dir_name = ARGV[0].to_s
 unless dir_name.empty?
@@ -14,7 +16,24 @@ image_files = Dir.glob "*.{png,bmp,svg,jpeg,jpg}"
 
 
 # A function to move the files to our new directory
+def move_files(file_type)
+  
+  # Get a string representation for our new directory
+  new_dir = Dir.pwd.to_s + "/" + file_type.to_s
 
+  # Create a new directory for our files
+  unless Dir.exist?(new_dir)
+    Dir.mkdir new_dir
+  end
+
+  # Move each file in our array to the new directory
+  FileUtils.mv(file_type, new_dir, secure: true)
+
+end
+
+
+
+move_files(compressed_files)
 
 
 # For Debugging
