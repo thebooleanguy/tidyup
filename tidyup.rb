6a3831d6fe8d372b4cabb0d489779dc85bb2677a
thebooleanguy <tharindus0601@gmail.com>
2024-12-20 09:18:25 +0530

    Add comments

--- tidyup.rb
+++ tidyup.rb
@@ -1,16 +1,25 @@
-# A program to sort your downloads folder
+# A program to organize your downloads directory by file type
 
-dirName = ARGV[0].to_s
-
-
-unless dirName.empty?
-  Dir.chdir dirName
+# Change working directory if argument provided
+dir_name = ARGV[0].to_s
+unless dir_name.empty?
+  Dir.chdir dir_name
 end
 
+# Detect files based on extension
 compressed_files = Dir.glob "*.{zip,rar,7z,tgz,gzip}"
 video_files = Dir.glob "*.{mp4,mkv,avi}"
 image_files = Dir.glob "*.{png,bmp,svg,jpeg,jpg}"
 
+
+
+# A function to move the files to our new directory
+
+
+
+# For Debugging
 #puts compressed_files
 #puts video_files
-puts image_files
+#puts image_files
+
+
